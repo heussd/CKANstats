88fcb70d63307bb4ab7523e02f2f0cb1e28778ad
Timm Heuss <Timm.Heuss AT web.de>
2015-03-10 11:52:32 +0100

    Introduced value-clean-function, CSV gets renamed automatically

--- CKANstats.py
+++ CKANstats.py
@@ -11,10 +11,23 @@ import pprint
 
 import csv
 
-package_list_url = 'http://demo.ckan.org/api/3/action/package_list'
+repository_url = "datahub.io" # <--- Change this
+delimiter = ','
+
+
+package_list_url = 'http://' + repository_url + '/api/3/action/package_list'
 
 # http://demo.ckan.org/api/3/action/package_search?q=name:ckandown
-package_search_url = 'http://demo.ckan.org/api/3/action/package_search'
+package_search_url = 'http://' + repository_url + '/api/3/action/package_search'
+
+
+
+# Removes non-ASCII / Delimiter characters
+def cleanFieldValue(fieldValue):
+	fieldValue = str(fieldValue)
+	fieldValue = fieldValue.encode("ascii", 'ignore')
+	fieldValue = fieldValue.replace(delimiter, "")
+	return fieldValue
 
 
 # Use the json module to dump a dictionary to a string for posting.
@@ -32,8 +45,8 @@ package_list = json.loads(list_response.read())
 assert package_list['success'] is True
 
 
-with open('ckanstats.csv', mode='wb') as csvfile:
-	csvwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+with open(repository_url + '.csv', mode='wb') as csvfile:
+	csvwriter = csv.writer(csvfile, delimiter=delimiter,quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
 	# dataset.id
 	# dataset.name
@@ -45,7 +58,7 @@ with open('ckanstats.csv', mode='wb') as csvfile:
 	# resource.revision_timestamp
 	# resource.format
 	# resource.url
-	csvwriter.writerow(['dataset.id','dataset.name','dataset.license_title','dataset.license_id','resource.id','resource.name','resource.created','resource.revision_timestamp','resource.format','resource.url'])
+	csvwriter.writerow(['dataset.id','dataset.name','dataset.license_title','dataset.license_id','dataset.isopen','dataset.tracking_summary.total','dataset.tracking_summary.recent','resource.id','resource.name','resource.created','resource.revision_timestamp','resource.format','resource.url'])
 
 	for package_name in package_list['result']:
 		data_string = urllib.quote(json.dumps({'q': ('name:' + package_name)}))
@@ -59,22 +72,26 @@ with open('ckanstats.csv', mode='wb') as csvfile:
 		assert search_result['result']['count'] is 1
 
 		for inner_result in search_result['result']['results']:
-			dataset_id = inner_result['id'] or ""
-			dataset_name = inner_result['name'] or ""
-			dataset_license_title = inner_result['license_title'] or ""
-			dataset_license_id = inner_result['license_id'] or ""
+			dataset_id = cleanFieldValue(inner_result['id'] or "n/a")
+			dataset_name = cleanFieldValue(inner_result['name'] or "n/a")
+			dataset_license_title = cleanFieldValue(inner_result['license_title'] or "n/a")
+			dataset_license_id = cleanFieldValue(inner_result['license_id'] or "n/a")
+			dataset_is_open = cleanFieldValue(inner_result['isopen'] or "False")
+			# http://docs.ckan.org/en/tracking-fixes/tracking.html#retrieving-tracking-data
+			dataset_tracking_summary_total = cleanFieldValue(inner_result['tracking_summary']['total'] or "0")
+			dataset_tracking_summary_recent = cleanFieldValue(inner_result['tracking_summary']['recent'] or "0")
 
 			print inner_result['name']
 
 			for resource in inner_result['resources']:
-				resource_id = resource['id'] or ""
-				resource_name = resource['name'] or ""
-				resource_created = resource['created'] or ""
-				resource_revision_timestamp = resource['revision_timestamp'] or ""
-				resource_format = resource['format'] or ""
-				resource_url = resource['url'] or ""
+				resource_id = cleanFieldValue(resource['id'] or "n/a")
+				resource_name = cleanFieldValue(resource['name'] or "n/a")
+				resource_created = cleanFieldValue(resource['created'] or "n/a")
+				resource_revision_timestamp = cleanFieldValue(resource['revision_timestamp'] or "n/a")
+				resource_format = cleanFieldValue(resource['format'] or "n/a")
+				resource_url = cleanFieldValue(resource['url'] or "n/a")
 
 
 				# Python 2's CSVWriter does not seem to support UTF-8 :/
 				# https://github.com/jdunck/python-unicodecsv
-				csvwriter.writerow([dataset_id.encode("ascii", 'ignore'),dataset_name.encode("ascii", 'ignore'),dataset_license_title.encode("ascii", 'ignore'),dataset_license_id.encode("ascii", 'ignore'),resource_id.encode("ascii", 'ignore'),resource_name.encode("ascii", 'ignore'),resource_created.encode("ascii", 'ignore'),resource_revision_timestamp.encode("ascii", 'ignore'),resource_format.encode("ascii", 'ignore'),resource_url.encode("ascii", 'ignore')])
+				csvwriter.writerow([dataset_id,dataset_name,dataset_license_title,dataset_license_id,dataset_is_open,dataset_tracking_summary_total,dataset_tracking_summary_recent,resource_id,resource_name,resource_created,resource_revision_timestamp,resource_format,resource_url])
